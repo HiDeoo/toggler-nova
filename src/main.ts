bc43653dcdcef6c4d193212a602101aa40f7844f
HiDeo <hideo.sup@gmail.com>
2020-11-03 12:33:08 +0100

    Add basic configuration loading

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,39 @@
-nova.commands.register('toggler.test', test)
+import defaults from './defaults'
 
-function test() {
-  console.log('plop')
+/**
+ * Toggler configuration.
+ */
+let configuration: ToggleConfiguration[] | undefined
+
+nova.commands.register('toggler.toggle', () => {
+  loadConfiguration()
+})
+
+/**
+ * Loads the configuration.
+ */
+function loadConfiguration() {
+  if (configuration) {
+    return
+  }
+
+  const useDefaultToggles = nova.config.get('toggler.useDefaultToggles', 'boolean') ?? true
+
+  let customToggles: ToggleConfiguration[] = []
+  const customTogglesStr = nova.config.get('toggler.toggles', 'string') ?? '[]'
+
+  try {
+    // TODO Validate & type this
+    customToggles = JSON.parse(customTogglesStr) as string[][]
+  } catch (error) {
+    // TODO
+    console.log('error ', error)
+  }
+
+  configuration = useDefaultToggles ? customToggles.concat(defaults) : customToggles
 }
+
+/**
+ * Toggle configuration.
+ */
+type ToggleConfiguration = string[]
