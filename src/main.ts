2e9c7d837fcd4f581e0677236c6eff29ab2960b6
HiDeo <hideo.sup@gmail.com>
2020-11-04 09:30:59 +0100

    Add configuration reloading

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,65 @@ import { Array, Static, String } from 'runtypes'
 import defaults from './defaults'
 
 /**
+ * Toggler configuration keys.
+ */
+enum TogglerConfiguration {
+  Toggles = 'toggler.toggles',
+  UseDefaultToggles = 'toggler.useDefaultToggles',
+}
+
+/**
  * Toggler configuration.
  */
 let configuration: ToggleConfiguration | undefined
 
+/**
+ * Event listeners used to listen to configuration changes.
+ */
+const configurationListeners: Disposable[] = []
+
+/**
+ * Toggle command.
+ */
 nova.commands.register('toggler.toggle', () => {
   void loadConfiguration()
 })
 
 /**
- * Loads the configuration.
+ * Triggered when the extension is activated.
  */
-async function loadConfiguration() {
-  if (configuration) {
+export function activate(): void {
+  nova.config.onDidChange(TogglerConfiguration.Toggles, () => {
+    void loadConfiguration(true)
+  })
+
+  nova.config.onDidChange(TogglerConfiguration.UseDefaultToggles, () => {
+    void loadConfiguration(true)
+  })
+}
+
+/**
+ * Triggered when the extension is deactivated.
+ */
+export function deactivate(): void {
+  configurationListeners.forEach((listener) => {
+    listener.dispose()
+  })
+}
+
+/**
+ * Loads the configuration.
+ * @param reload - Defines if the configuration should be reloaded or not.
+ */
+async function loadConfiguration(reload = false) {
+  if (configuration && !reload) {
     return
   }
 
-  const useDefaultToggles = nova.config.get('toggler.useDefaultToggles', 'boolean') ?? true
+  const useDefaultToggles = nova.config.get(TogglerConfiguration.UseDefaultToggles, 'boolean') ?? true
 
   let customToggles: ToggleConfiguration = []
-  const customTogglesStr = nova.config.get('toggler.toggles', 'string') ?? '[]'
+  const customTogglesStr = nova.config.get(TogglerConfiguration.Toggles, 'string') ?? '[]'
 
   try {
     const parsedCustomToggles: unknown = JSON.parse(customTogglesStr)
